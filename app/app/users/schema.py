98a2192f44dff5e83e403e7636cc4782b7878883
abhijith <abhijithelampal005@gmail.com>
2021-04-24 21:37:22 +0530

    add user and schema

---
+++ app/app/users/schema.py
@@ -0,0 +1,30 @@
+from django.contrib.auth import get_user_model
+import graphene
+from graphene_django import DjangoObjectType
+from typing_extensions import Required
+
+
+class UserType(DjangoObjectType):
+    class Meta:
+        model = get_user_model()
+
+
+class CreateUser(graphene.Mutation):
+    user = graphene.Field(UserType)
+
+    class Arguments:
+        username = graphene.String(required=True)
+        password = graphene.String(required=True)
+        email = graphene.String(required=True)
+
+    def mutate(self, info, username, password, email):
+        user = get_user_model()(
+            username=username,
+            email=email
+        )
+        user.set_password(password)
+        user.save()
+
+
+class Mutation(graphene.ObjectType):
+    create_user = CreateUser.Field()
