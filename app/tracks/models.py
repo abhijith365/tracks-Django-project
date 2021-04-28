df877958e46266fd97b48a5e7dc62aa380d1c103
abhijith <abhijithelampal005@gmail.com>
2021-04-28 23:19:12 +0530

    added posted_by query

--- app/tracks/models.py
+++ app/tracks/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+from django.contrib.auth import get_user_model
 # Create your models here.
 
 
@@ -8,3 +9,5 @@ class Tracks(models.Model):
     description = models.TextField(blank=True)
     url = models.URLField()
     created_at = models.DateField(auto_now_add=True)
+    posted_by = models.ForeignKey(
+        get_user_model(), null=True, on_delete=models.CASCADE)
